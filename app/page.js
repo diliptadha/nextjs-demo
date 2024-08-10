8eecfa868e8527af197ffdce87e56f127526e8c2
diliptadha <diliptadha098@gmail.com>
2024-08-10 15:16:38 +0530

    changes

--- app/page.js
+++ app/page.js
@@ -47,7 +47,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2>
-            Docs <span>-&gt;</span>
+            dilip <span>-&gt;</span>
           </h2>
           <p>Finddilip in-depth information about Next.js features and API.</p>
         </a>
